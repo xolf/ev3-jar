2a7c028d400354cbb64b15a76a268933fdb45fbc
Florian <xolf@users.noreply.github.com>
2015-04-28 15:45:09 +0200

    0.0.2

--- test.java
+++ test.java
@@ -10,6 +10,22 @@ class Bot
         robot.addPart(gear);
         gear.forward(ms);
         robot.exit();
+    } 
+    turnLeft(int ms)
+    {
+        LegoRobot robot = new LegoRobot();
+        Gear gear = new Gear();
+        robot.addPart(gear);
+        gear.left(ms);
+        robot.exit();
+    }
+    turnRight(int ms)
+    {
+        LegoRobot robot = new LegoRobot();
+        Gear gear = new Gear();
+        robot.addPart(gear);
+        gear.left(ms);
+        robot.exit();
     }
 
     public static void main(String[] args)
