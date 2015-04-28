b82f0208cea244c583cc0a57b70e6ecdcf7b4214
Florian <xolf@users.noreply.github.com>
2015-04-28 17:04:30 +0200

    Update fun.java

--- fun.java
+++ fun.java
@@ -1,1 +1,20 @@
-//Include main.java
+import ch.aplu.ev3.*;
+
+class Fun
+{
+
+    Fun()
+      {
+        LegoRobot robot = new LegoRobot();
+        Motor motA = new Motor(MotorPort.A);
+        robot.addPart(motA);
+        motA.forward();
+        Tools.delay(1000);
+        robot.exit();
+    }
+    
+    public static void main(String[] args)
+    {
+        new Fun();
+    }
+}
